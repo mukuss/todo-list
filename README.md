6274965e1c57ab7816d61651431bfcaef82bb97c
mukuss <mukuss@qq.com>
2020-12-31 16:16:58 +0800

    完成第二阶段用户登录功能

--- README.md
+++ README.md
@@ -6,16 +6,22 @@
 
 ## 说明
 
-这是一个代办事项管理脚本，现已实现第一阶段功能，包含 add、done、list三个子功能 
+这是一个代办事项管理脚本，现已实现第二阶段功能，包含 add、done、list、login、logout、register六个子功能 
 
-其中add用以增加代办事项，done用以将代办标记完成，list用以打印代办事项，可以使用--all参数可打印所有事项
+其中add用以增加代办事项，done用以将代办标记完成，list用以打印代办事项，可以使用--all参数可打印所有事项，
+register用以注册账户，使用`register [-u 用户名]`根据提示输入用户密码即可注册，
+login用以登录使用，使用`login [-u 用户名]`根据提示输入用户密码即可登录，
+logout用以退出登录，
 
-文件保存在用户根目录文件夹中的.todo_data文件中，以json格式保存，后附md5校验以判断数据正确性
+文件保存在用户根目录文件夹中的.todo-data文件中，以json格式保存，后附md5校验以判断数据正确性
+用户和登录信息保存在用户根目录文件夹中的.todo-config文件中，以json格式保存，后附md5校验以判断数据正确性
+用户密码以sha256散列保存，初步保护用户密码信息
 
 ## 版本升级：
 
 1. 2020-12-30 第一版功能实现
-2. 2020-12-31 增加说明、增加数据文件md5校验、整理代码并去除代码中部分“坏味道”使其更易于维护
+1. 2020-12-31 增加说明、增加数据文件md5校验、整理代码并去除代码中部分“坏味道”使其更易于维护
+1. 2020-12-31 完成第二阶段用户登录功能
 
 ## 环境搭建
 
@@ -33,15 +39,22 @@
 
 ### 获取作业版本
 
-`git clone --branch todo-phase-1 https://github.com/mukuss/todo-list.git`
+`git clone --branch todo-phase-2 https://github.com/mukuss/todo-list.git`
 
 ### 运行测试
 
-1. `python todo.py add "hello wold"`
-2. `python todo.py add "todo-phase-2"`
-3. `python todo.py list`
-4. `python todo.py done 2`
-5. `python todo.py list`
-6. `python todo.py list --all`
+1. `python todo.py register -u mukuss`#输入两遍任意密码注册
+1. `python todo.py login -u mukuss`#输入刚刚注册的密码
+1. `python todo.py add "todo-phase-1"`
+1. `python todo.py add "todo-phase-2"`
+1. `python todo.py add "todo-phase-3"`
+1. `python todo.py add "todo-phase-4"`
+1. `python todo.py done 1`
+1. `python todo.py done 2`
+1. `python todo.py list --all`
+1. `python todo.py list`
+1. `python todo.py logout`
+1. `python todo.py list`
+
 
 
--- todo.py
+++ todo.py
@@ -2,11 +2,51 @@ import json
 import hashlib
 import argparse
 import os
+import getpass
 
 
 class todo():
+    def _read_todo_config_file(self):
+        file_name = os.path.join(os.path.expanduser('~'), '.todo-config')
+        try:
+            f = open(file_name, 'r')
+            file_content = f.read()
+            if file_content[-32:] != hashlib.md5(bytes(file_content[:-32], encoding='utf8')).hexdigest():
+                raise IOError("Config file validation error")
+            f.close()
+            return file_content[:-32]
+        except (FileNotFoundError, IOError):
+            raise IOError("Config file validation error")
+
+    def _read_and_parse_config_file(self):
+        try:
+            data = json.loads(self._read_todo_config_file())
+        except (IOError, json.decoder.JSONDecodeError):
+            init_data = {
+                "active_user": None,
+                "users": {},
+            }
+            print("Initializes the config file")
+            data = init_data 
+        return data
+
+    def _write_todo_config_file(self, data):
+        file_name = os.path.join(os.path.expanduser('~'), '.todo-config')
+        try:
+            with open(file_name, 'w') as f:
+                f.write(data + hashlib.md5(bytes(data, encoding='utf8')).hexdigest())
+        except PermissionError as e:
+            raise IOError(
+                "Error: Unable to open file %s, unblock or grant read/write access to the file" % file_name)
+
+    def _pack_and_write_config_file(self,data):
+        try:
+            self._write_todo_config_file(json.dumps(data))
+        except IOError as e:
+            print(str(e))
+
     def _read_todo_data_file(self):
-        file_name = os.path.join(os.path.expanduser('~'), '.todo_data')
+        file_name = os.path.join(os.path.expanduser('~'), '.todo-data')
         try:
             f = open(file_name, 'r')
             file_content = f.read()
@@ -15,10 +55,20 @@ class todo():
             f.close()
             return file_content[:-32]
         except (FileNotFoundError, IOError):
-            raise IOError("file validation error")
+            raise IOError("Data file validation error")
+
+    def _read_and_parse_data_file(self):
+        try:
+            data = json.loads(self._read_todo_data_file())
+        except (IOError, json.decoder.JSONDecodeError):
+            init_data = {
+            }
+            print("Initializes the data file")
+            data = init_data 
+        return data
 
     def _write_todo_data_file(self, data):
-        file_name = os.path.join(os.path.expanduser('~'), '.todo_data')
+        file_name = os.path.join(os.path.expanduser('~'), '.todo-data')
         try:
             with open(file_name, 'w') as f:
                 f.write(data + hashlib.md5(bytes(data, encoding='utf8')).hexdigest())
@@ -26,35 +76,29 @@ class todo():
             raise IOError(
                 "Error: Unable to open file %s, unblock or grant read/write access to the file" % file_name)
 
-    def _read_and_parse_data_file(self):
-        try:
-            data = json.loads(self._read_todo_data_file())
-        except (IOError, json.decoder.JSONDecodeError):
-            init_data = {
-                'active_user': 'default',
-                'users': {
-                    'default': {
-                        'items': [],
-                    },
-                },
-            }
-            print("Initializes the data file")
-            data = init_data 
-        return data
-
     def _pack_and_write_data_file(self,data):
         try:
             self._write_todo_data_file(json.dumps(data))
         except IOError as e:
             print(str(e))
 
+    def _import_user_data(self):
+        self.user = self.config['active_user']
+        if self.user != None:
+            if self.user not in self.file_data.keys():
+                self.file_data[self.user] = {'items': [], }
+            self.items = self.file_data[self.user]['items']
+        else:
+            print('Please log in')
+            exit()
+
     def __init__(self):
         self.file_data = self._read_and_parse_data_file()
-        active_user = self.file_data['active_user']
-        self.items = self.file_data['users'][active_user]['items']
+        self.config = self._read_and_parse_config_file()
 
     def __del__(self):
         self._pack_and_write_data_file(self.file_data)
+        self._pack_and_write_config_file(self.config)
 
     def _print_items(self, print_all):
         print('')
@@ -68,6 +112,7 @@ class todo():
         print('')
 
     def add(self, item):
+        self._import_user_data()
         itemIndex = len(self.items)+1
         self.items.append(
             {'index': itemIndex, 'text': item, 'state': 'undone'})
@@ -75,11 +120,13 @@ class todo():
         print('Item %d added' % itemIndex)
 
     def done(self, itemIndex):
+        self._import_user_data()
         self.items[itemIndex-1]['state'] = 'done'
         self._print_items(False)
         print('Item %d done' % itemIndex)
 
     def list(self, list_all):
+        self._import_user_data()
         self._print_items(list_all)
         items_number = len(self.items)
         done_number = len(
@@ -91,6 +138,41 @@ class todo():
         else:
             print('Total: %d items, %d item done' %
                   (items_number, done_number))
+
+    def register(self, user_name):
+        if user_name == None:
+            user_name = input("register user name:")
+        password = getpass.getpass(prompt="password:")
+        retype_password = getpass.getpass(prompt="retype password:")
+        if password != retype_password:
+            print("The two passwords are inconsistent. Please register again")
+            exit()
+        if user_name in self.config['users'].keys():
+            print("registration failed! This user name has been registered")
+            exit()
+        password = hashlib.sha256(bytes(password, encoding='utf8')).hexdigest() 
+        self.config['users'][user_name] = password
+        print("Register successfully, log in and start using")
+    def login(self,user_name):
+        if user_name == None:
+            user_name = input("user:")
+        password = getpass.getpass()
+        password = hashlib.sha256(bytes(password, encoding='utf8')).hexdigest() 
+        if user_name not in self.config['users'].keys():
+            print("Longin failed!")
+            exit()
+        if password != self.config['users'][user_name]:
+            print("Longin failed!")
+            exit()
+
+        if self.config['active_user'] != None:
+            print("User %d has logged out" % self.config['active_user'])
+        self.config['active_user'] = user_name
+        print("Login success!")
+    def logout(self):
+        self.config['active_user'] = None
+        print("Logout success!")
+        pass
 
 
 # 程序从这里开始，主要为使用argparse处理输入参数，业务代码均在todo类中
@@ -112,6 +194,18 @@ if(__name__ == '__main__'):
     parser_list.add_argument('-a', '--all', nargs='?',
                              default=False, const=True, help='')
     parser_list.set_defaults(func=lambda args: todo().list(args.all))
+
+    parser_register = subparsers.add_parser('register')
+    parser_register.add_argument('-u', '--user', nargs='?', default=None, help='')
+    parser_register.set_defaults(func=lambda args: todo().register(args.user))
+
+    parser_login = subparsers.add_parser('login')
+    parser_login.add_argument('-u', '--user', nargs='?', default=None, help='')
+    parser_login.set_defaults(func=lambda args: todo().login(args.user))
+
+    parser_logout = subparsers.add_parser('logout')
+    parser_logout.set_defaults(func=lambda args: todo().logout())
+
 
     try:
         args = parser.parse_args()
